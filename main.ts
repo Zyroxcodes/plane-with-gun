87898ce587a5ed04a0178566cc8f75d7e7a8cf60
Zyroxdev <79484310+Zyroxcodes@users.noreply.github.com>
2021-03-22 18:46:49 +0000

    better

--- main.ts
+++ main.ts
@@ -1,35 +1,26 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    
-    projectile = sprites.createProjectileFromSprite(assets.image`
-        laser1
-    `, mySprite, 300, 0)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`laser1`, mySprite, 300, 0)
+    music.pewPew.play()
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy(effects.fire, 500)
     info.changeScoreBy(1)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap2(sprite: Sprite, otherSprite: Sprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
     info.changeLifeBy(-1)
     scene.cameraShake(4, 500)
 })
-let bogey : Sprite = null
-let projectile : Sprite = null
-let mySprite : Sprite = null
-scene.setBackgroundImage(assets.image`
-    background 1
-`)
-mySprite = sprites.create(assets.image`
-    plane
-`, SpriteKind.Player)
+let bogey: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundImage(assets.image`background 1`)
+mySprite = sprites.create(assets.image`plane`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
 info.setLife(3)
-game.onUpdateInterval(500, function on_update_interval() {
-    
-    bogey = sprites.create(assets.image`
-        bogey1
-    `, SpriteKind.Enemy)
+game.onUpdateInterval(500, function () {
+    bogey = sprites.create(assets.image`bogey1`, SpriteKind.Enemy)
     bogey.setVelocity(-100, 0)
     bogey.setPosition(160, randint(5, 115))
     bogey.setFlag(SpriteFlag.AutoDestroy, true)
